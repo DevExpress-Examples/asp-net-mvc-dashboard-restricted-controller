435ecc8bfe9bb03982cd23e705c0a66e3ab7b3f7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXWebApplication1/Controllers/MyDashboardController.cs
@@ -0,0 +1,13 @@
+﻿using DevExpress.DashboardWeb.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace DXWebApplication1.Controllers
+{
+    public class MyDashboardController : DashboardController
+    {
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to create a custom dashboard controller
+
+
+This example illustrates how to create a custom dashboard controller for <a href="https://documentation.devexpress.com/#Dashboard/clsDevExpressDashboardWebASPxDashboardtopic">ASPxDashboard</a> in an MVC solution. 
+
+<br/>
+
+
